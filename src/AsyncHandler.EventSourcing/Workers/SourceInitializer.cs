b6e471ebb4d7942b3455227ae0ada2cb74acc148
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-08-29 00:54:03 +0300

    Block source init and wait for completion

--- src/AsyncHandler.EventSourcing/Workers/SourceInitializer.cs
+++ src/AsyncHandler.EventSourcing/Workers/SourceInitializer.cs
@@ -1,19 +1,17 @@
-using AsyncHandler.EventSourcing.Configuration;
+using AsyncHandler.EventSourcing.Repositories;
 using Microsoft.Extensions.Hosting;
 
 namespace AsyncHandler.EventSourcing.Workers;
 
-public class SourceInitializer(IRepository<AggregateRoot> repository, EventSources source) : IHostedService
+internal class SourceInitializer(EventSource<AggregateRoot> eventSource) : IHostedService
 {
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        await Task.Run( () => source switch
-        {
-            EventSources.AzureSql => repository.AzureSqlClient.InitSource(),
-            EventSources.PostgresSql => repository.PostgreSqlClient.InitSource(),
-            EventSources.SQLServer => repository.SqlServerClient.InitSource(),
-            _ => Task.CompletedTask,
-        });
+        // await Task.Run( () =>
+        // {
+        //     eventSource.InitSource();
+        // }, cancellationToken);
+        await eventSource.InitSource();
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
