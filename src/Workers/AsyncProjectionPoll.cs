a20ad3a667cc433d9b6cf1ced39101cc899cc577
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-11-16 15:56:48 +0300

    add initial async projection engine

--- src/Workers/AsyncProjectionPoll.cs
+++ src/Workers/AsyncProjectionPoll.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using EventStorage.Events;
 using Microsoft.Extensions.Logging;
 
 namespace EventStorage.Workers;
@@ -7,10 +8,12 @@ public interface IAsyncProjectionPoll
 {
     Task PollAsync(CancellationToken token);
     void Release();
+    ConcurrentQueue<IEnumerable<SourcedEvent>> ProjectionsQueue {  get; }
 }
 public class AsyncProjectionPoll : IAsyncProjectionPoll
 {
     private readonly SemaphoreSlim _pool = new(1, 1);
+    public ConcurrentQueue<IEnumerable<SourcedEvent>> ProjectionsQueue => [];
     public async Task PollAsync(CancellationToken token) => await _pool.WaitAsync(token);
     public void Release() => _pool.Release();
 }
