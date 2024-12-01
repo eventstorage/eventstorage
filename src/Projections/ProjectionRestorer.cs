a5b086a3c19040f13c55ae7523acae99132c0b6f
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-12-02 00:43:52 +0300

    make projecton restoer generic

--- src/Projections/ProjectionRestorer.cs
+++ src/Projections/ProjectionRestorer.cs
@@ -1,28 +1,23 @@
 using System.Reflection;
 using EventStorage.AggregateRoot;
 using EventStorage.Events;
+using EventStorage.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace EventStorage.Projections;
 
-public class ProjectionRestorer(
-    IServiceProvider sp, Dictionary<IProjection, List<MethodInfo>> projections) : IProjectionRestorer
+public class ProjectionRestorer<T>(IServiceProvider sp) : IProjectionRestorer<T> where T : IEventSource
 {
-    private readonly ILogger logger = new LoggerFactory().CreateLogger<ProjectionRestorer>();
-    public object? Project(IProjection projection, IEnumerable<SourcedEvent> events, Type model) =>
+    private readonly ILogger logger = TLogger.Create<ProjectionRestorer<T>>();
+    public object? Project(IProjection<T> projection, IEnumerable<SourcedEvent> events, Type model) =>
         Project(events, projection, model);
-    public object? ProjectOptimized(IProjection projection, IEnumerable<SourcedEvent> events, Type model) =>
-        ProjectPreCompiled(events, projection, model);
     public M? Project<M>(IEnumerable<SourcedEvent> events) =>
-        (M?) Project(events, sp.GetRequiredService<IProjection<M>>(), typeof(M));
-    public M? ProjectOptimized<M>(IEnumerable<SourcedEvent> events) =>
-        (M?) ProjectPreCompiled(events, sp.GetRequiredService<IProjection<M>>(), typeof(M));
-    public bool Subscribes(IEnumerable<SourcedEvent> events, IProjection projection) =>
+        (M?) Project(events, sp.GetRequiredService<IProjection<M,T>>(), typeof(M));
+    public bool Subscribes(IEnumerable<SourcedEvent> events, IProjection<T> projection) =>
         projection.GetType().GetMethods().Where(m => m.Name == "Project")
         .Any(m => events.Any(e =>  e.GetType().IsAssignableFrom(m.GetParameters()
-        .FirstOrDefault(x => typeof(SourcedEvent).IsAssignableFrom(x.ParameterType))?.ParameterType)
-        ));
+        .FirstOrDefault(x => typeof(SourcedEvent).IsAssignableFrom(x.ParameterType))?.ParameterType)));
     private object? Project(IEnumerable<SourcedEvent> events, object projection, Type model)
     {
         try
@@ -47,30 +42,6 @@ public class ProjectionRestorer(
         {
             if(logger.IsEnabled(LogLevel.Error))
                 logger.LogError($"Failure restoring {model.Name}.{Environment.NewLine}{e.Message}");
-            throw;
-        }
-    }
-    private object? ProjectPreCompiled(IEnumerable<SourcedEvent> events, object projection, Type model)
-    {
-        try
-        {
-            var methods = projections[(IProjection)projection];
-            var record = methods.First().Invoke(projection, [events.First()]);
-            foreach (var e in events.Skip(1))
-            {
-                var method = methods.FirstOrDefault(m => 
-                    m.GetParameters().First().ParameterType.IsAssignableFrom(record?.GetType()) &&
-                    m.GetParameters().Last().ParameterType.IsAssignableFrom(e.GetType()));
-                if (method == null)
-                    continue;
-                record = method.Invoke(projection, [record, e]);
-            }
-            return record;
-        }
-        catch (TargetInvocationException e)
-        {
-            if(logger.IsEnabled(LogLevel.Error))
-                logger.LogError($"Projection failure for {model.Name}. {e.Message}");
             throw;
         }
     }
