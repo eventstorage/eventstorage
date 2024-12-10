94158271d4e2f1942d67f8e541811c08c7660d5e
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-12-10 18:45:44 +0300

    Add providers to TLogger

--- src/Extensions/TLogger.cs
+++ src/Extensions/TLogger.cs
@@ -4,6 +4,10 @@ namespace EventStorage.Extensions;
 
 public static class TLogger
 {
-    private static readonly LoggerFactory _factory = new();
+    private static ILoggerFactory _factory = LoggerFactory.Create(builder =>
+    {
+        builder.AddConsole()
+        .AddDebug();
+    });
     public static ILogger Create<T>() => _factory.CreateLogger<T>();
 }
